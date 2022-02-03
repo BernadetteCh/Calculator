23c6aed20d3422ad3ba031049653bc4be4ac304b
BernadetteCh <b.christiner93@gmail.com>
2022-02-03 21:22:06 +0100

    created function removeZero to not display 0, whenever a button gets clicked

--- java.js
+++ java.js
@@ -1,11 +1,12 @@
 function display(val) {
-  var x= document.getElementById("output")+=val;//+=it allows you to add something to a variable and store the result in that variable,
-
-  /* var calculation = document.getElementById("output");
-   calculation.value+=val;*/
-  //i wähl erstaml des id Element aus wo derRechnung angezeigt werden soll
-  //dann erst geb i der Varaible ein neues Value hinzu nämlich mit dem val Parameter;
+  removeZero();
+  document.getElementById("output").innerHTML += val; //+=it allows you to add something to a variable and store the result in that variable;
 }
+
+/* var calculation = document.getElementById("output");
+   calculation.value+=val;*/
+//i wähl erstaml des id Element aus wo derRechnung angezeigt werden soll
+//dann erst geb i der Varaible ein neues Value hinzu nämlich mit dem val Parameter;
 
 function solveCalculation() {
   var calculation = document.getElementById("output");
@@ -26,10 +27,23 @@ function removeCalculation() {
 }
 
 function calculatePerc() {
- 
-var percentage=document.getElementById("output");
-var result=percentage.innerHTML/100;
-percentage.innerHTML=result;
+  var percentage = document.getElementById("output");
+  var result = percentage.innerHTML / 100;
+  percentage.innerHTML = result;
+}
 
+//inner.HTML vo output ist bei default 0
+//lös i a function aus jedesmal wenn i an button klicke
+//mit diesem klick übermitlle ich mit an parameter an Wert
+//dieser Wert erscheint dann im output mit dem Wert den es bei default hat
 
+//! inner.html vo output soll verschwinden sobald i an button klicke und a Parameter übermittle
+
+function removeZero() {
+  var defaultvalue = document.getElementById("output").innerHTML;
+  if (defaultvalue == 0) {
+    defaultvalue = "";
+    var x = document.getElementById("output");
+    x.innerHTML = defaultvalue;
+  }
 }

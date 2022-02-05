384d15c1528b07c4eacd9ec089b19b36106964d0
BernadetteCh <b.christiner93@gmail.com>
2022-02-05 21:43:11 +0100

    created function to calculate with negative sign and removed a comment

--- java.js
+++ java.js
@@ -1,25 +1,14 @@
 function display(val) {
   removeZero();
+  calculateMinus();
   document.getElementById("output").innerHTML += val; //+=it allows you to add something to a variable and store the result in that variable;
 }
-
-/* var calculation = document.getElementById("output");
-   calculation.value+=val;*/
 //i wähl erstaml des id Element aus wo derRechnung angezeigt werden soll
 //dann erst geb i der Varaible ein neues Value hinzu nämlich mit dem val Parameter;
 
 function solveCalculation() {
   var calculation = document.getElementById("output");
-  calculation.innerHTML = eval(calculation.innerHTML);
-  /* 
-  let x = document.getElementById("output").value; //ich weiße dem output quasi an leeren Wert zu
-  console.log(x);//hier erkennt es 1+2
-  let y = eval(x); // eval() -> Wandelt Ausdruck in Ergebnis um
-  console.log(y);//hier erkennt er 3 
-  let result=document.getElementById("output");
-  result.value=y;
-  console.log(result);
-*/
+  calculation.innerHTML = eval(calculation.innerHTML); //eval() -> Wandelt Ausdruck in Ergebnis um
 }
 
 function removeCalculation() {
@@ -39,4 +28,16 @@ function removeZero() {
     var x = document.getElementById("output");
     x.innerHTML = defaultvalue;
   }
+}
+
+function calculateMinus() {
+  var v = document.querySelector(".btn-minus");
+  var x = document.getElementById("output").innerHTML;
+  if (v) {
+    var y = x * "-1";
+      console.log(y);
+  
+  }
+
+
 }
